422a71113eb06564321f583dbc0d3d0cba119654
stackCuibin <bb.cui@foxmail.com>
2019-08-02 00:03:54 +0800

    StaticList 和 DynamicList的实现

--- project/Exception.h
+++ project/Exception.h
@@ -48,6 +48,24 @@ public:
     }
 };
 
+class IndexOutOfBoundsException : public Exception
+{
+public:
+	IndexOutOfBoundsException() : Exception(0) {}
+	IndexOutOfBoundsException(const char* message) : Exception(message) {}
+	IndexOutOfBoundsException(const char* file, int line) : Exception(file, line) {}
+	IndexOutOfBoundsException(const char* message, const char* file, int line) : Exception(message, file, line) {}
+
+	IndexOutOfBoundsException(const IndexOutOfBoundsException& e) : Exception(e) {}
+
+	IndexOutOfBoundsException& operator = (const IndexOutOfBoundsException& e)
+	{
+		Exception::operator=(e);
+
+		return *this;
+	}
+};
+
 class InvalidOperationException : public Exception
 {
 public:
--- project/main.cpp
+++ project/main.cpp
@@ -8,19 +8,75 @@
 #include <iostream>
 #include "SmartPointer.h"
 #include "Exception.h"
+#include "StaticList.h"
+#include "DynamicList.h"
 
 using namespace std;
 using namespace DTLib;
 
 int main(int argc, const char* argv[])
 {
+	cout << "SmartPointer test begin ..." << endl;
     SmartPointer<int>* sp = new SmartPointer<int>();
 
     delete sp;
+	cout << "SmartPointer test end" << endl << endl;
 
+	cout << "Exception test begin ..." << endl;
     InvalidOperationException* e = new InvalidOperationException(0);
 
     delete e;
+	cout << "Exception test end" << endl << endl;
+
+	cout << "StaticList test begin ..." << endl;
+	StaticList<int, 5> sl;
+	for(int i = 0; i < sl.capacity(); i++)
+	{
+		sl.insert(0, i);
+	}
+	for(int i = 0; i < sl.length(); i++)
+	{
+		cout << sl[i] << endl;
+	}
+	try
+	{
+		sl[5] = 5;
+	}
+	catch(const Exception& e)
+	{
+		cout << e.message() << endl;
+		cout << e.location() << endl;
+	}
+	cout << "StaticList test end" << endl << endl;
+
+	cout << "DynamicList test begin ..." << endl;
+	DynamicList<int> dl(5);
+	for(int i = 0; i < dl.capacity(); i++)
+	{
+		dl.insert(0, i);
+	}
+	for(int i = 0; i < dl.length(); i++)
+	{
+		cout << dl[i] << endl; 
+	}
+	try
+	{
+		dl[5] = 5;
+	}
+	catch(const Exception& e)
+	{
+		cout << e.message() << endl;
+		cout << e.location() << endl;
+
+		dl.resize(10);
+		dl.insert(5, 10);
+	}
+	dl[5] = 5;
+	for(int i = 0; i < dl.length(); i++)
+	{
+		cout << dl[i] << endl;
+	}
+	cout << "DynamicList test end" << endl << endl;
 
     return 0;
 }
