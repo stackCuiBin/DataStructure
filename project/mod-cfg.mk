18414bd868b535769257ab1df1a9c9548cd5f4be
stackCuibin <bb.cui@foxmail.com>
2021-07-11 23:16:34 +0800

    调整目录结构，添加makefile

---
+++ project/mod-cfg.mk
@@ -0,0 +1,9 @@
+
+DIR_SRC := src
+DIR_INC := inc
+
+TYPE_INC := .h
+TYPE_SRC := .cpp
+TYPE_OBJ := .o
+TYPE_DEP := .dep
+
---
+++ project/pro-cfg.mk
@@ -0,0 +1,16 @@
+
+MODULES := DTLib \
+		   main
+
+MOD_CFG := mod-cfg.mk
+MOD_RULE := mod-rule.mk
+CMD_CFG := cmd-cfg.mk
+
+DIR_PROJECT := $(realpath .)
+DIR_BUILD := build
+DIR_COMMON_INC := DTLib/inc
+DIR_LIBS_INC := 
+DIR_LIBS_LIB := libs/lib
+
+APP := app.out
+
